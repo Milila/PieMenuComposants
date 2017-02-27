29560571763d10ae9083859a1925ca7e4be29bc9
Elodie Bouzekri <elodie.bouzekri@alumni.enac.fr>
2017-02-27 17:56:33 +0100

    Composant item.

--- src/piemenucomposants/ItemPieMenu.java
+++ src/piemenucomposants/ItemPieMenu.java
@@ -6,83 +6,206 @@
 package piemenucomposants;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JComponent;
 
 /**
  *
  * @author cannacan
  */
-public class ItemPieMenu {
-    
-    
+public class ItemPieMenu extends JComponent {
+
     //IL MANQUE LES ACTIONS ASSOCIEES A CHAQUE ITEM!!!
-    
     private static final String PROPERTY_TEXT = "text";
     private static final String PROPERTY_BACKGROUND = "background";
     private static final String PROPERTY_FOREGROUND = "foreground";
+    private static final String PROPERTY_WIDTH = "width";
+    private static final String PROPERTY_HEIGTH = "heigth";
+    private static final String PROPERTY_NUM_QUARTIER = "numquartier";
     private PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private List<PieMenuListener> itemListeners;
     
+    private boolean armed;
+    private int widthPie = 10;
+    private int heigthPie = 10;
+    private int num_quartier = 0;
+
     private String text;
     private Color background;
     private Color foreground;
     private int angle;
-    
-    public ItemPieMenu (String t, Color b, Color f){
+
+    public ItemPieMenu() {
+        this("", Color.BLACK, Color.BLUE);
+    }
+
+    public ItemPieMenu(String t, Color b, Color f) {
         this.text = t;
         this.background = b;
         this.foreground = f;
+        this.itemListeners = new ArrayList();
         
         this.privateAddPropertyChange(PROPERTY_TEXT, new PropertyChangeListener() {
             @Override
             public void propertyChange(PropertyChangeEvent evt) {
-                //DO SOMETHING
+                 repaint();
             }
         });
         this.privateAddPropertyChange(PROPERTY_BACKGROUND, new PropertyChangeListener() {
             @Override
             public void propertyChange(PropertyChangeEvent evt) {
-                //DO SOMETHING
+                repaint();
             }
         });
         this.privateAddPropertyChange(PROPERTY_FOREGROUND, new PropertyChangeListener() {
             @Override
             public void propertyChange(PropertyChangeEvent evt) {
-                //DO SOMETHING
+                 repaint();
             }
         });
+        this.privateAddPropertyChange(PROPERTY_HEIGTH, new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                repaint();
+            }
+        });
+        this.privateAddPropertyChange(PROPERTY_NUM_QUARTIER, new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                repaint();
+            }
+        });
+        this.privateAddPropertyChange(PROPERTY_WIDTH, new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                repaint();
+            }
+        });
+        
+        this.addMouseListener(new MouseAdapter() {
+            private Color color = getBackground();
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(color);
+                //armed = false;
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                armed = true;
+                setBackground(Color.BLUE.darker());
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                fireItemClicked(new PieMenuEvent(this));
+                System.out.println("CLICK");
+            }
+
+        });
+       
     }
     
-    
-    public void setText (String t){
+     private void fireItemClicked(PieMenuEvent itemEvent) {
+        for (PieMenuListener listener : itemListeners) {
+            listener.pieMenuMouseDown(itemEvent);
+        }
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        System.out.println(widthPie+" "+heigthPie+" "+angle+" "+num_quartier);
+        Graphics2D g2 = (Graphics2D) graphics;
+        //g2.setColor(background);
+        Color oldColor = graphics.getColor();
+        g2.setColor(background);
+        g2.fillArc(0, 0, widthPie, heigthPie, angle * num_quartier, angle);
+
+        //Tracé du cercle
+        int d = widthPie / 2;
+        int x = (int) ((widthPie * 1 / 3) * Math.cos(Math.toRadians((-1) * (angle * (2 * num_quartier + 1)) / 2)));
+        int y = (int) ((d * 2 / 3) * Math.sin(Math.toRadians((-1) * (angle * (2 * num_quartier + 1)) / 2)));
+        g2.setColor(foreground);
+        g2.drawString(text, x + d, y + d);
+        g2.setColor(oldColor);
+    }
+
+    public int getWidthPie() {
+        return widthPie;
+    }
+
+    public void setWidthPie(int widthPie) {
+        int oldWidth = this.widthPie;
+        this.widthPie = widthPie;
+        support.firePropertyChange(PROPERTY_WIDTH, oldWidth, widthPie);
+    }
+
+    public int getHeigthPie() {
+        return heigthPie;
+    }
+
+    public void setHeigthPie(int heigthPie) {
+        int oldHeigth = this.heigthPie;
+        this.heigthPie = heigthPie;
+        support.firePropertyChange(PROPERTY_HEIGTH, oldHeigth, heigthPie);
+    }
+
+    public int getNum_quartier() {
+        return num_quartier;
+    }
+
+    /*
+    public void draw(Graphics2D g2, int widthPie, int heigthPie, int depangle, int endangle) {
+    g2.setColor(background);
+    g2.fillArc(0, 0, widthPie, heigthPie, depangle, endangle);   
+    }
+     */
+    public void setNum_quartier(int num_quartier) {
+        int oldNum_quartier = this.num_quartier;
+        this.num_quartier = num_quartier;
+        support.firePropertyChange(PROPERTY_NUM_QUARTIER, oldNum_quartier, this.num_quartier);
+    }
+
+    public void setText(String t) {
         String oldText = this.text;
         this.text = t;
         support.firePropertyChange(PROPERTY_TEXT, oldText, t);
     }
-    public String getText(){
+
+    public String getText() {
         return this.text;
     }
-    
-    public void setBackground(Color b){
+
+    public void setBackground(Color b) {
         Color oldColor = this.background;
         this.background = b;
         support.firePropertyChange(PROPERTY_BACKGROUND, oldColor, b);
     }
-    public Color getBackground(){
+
+    public Color getBackground() {
         return this.background;
     }
-    
-    
-    public void setForeground(Color f){
+
+    public void setForeground(Color f) {
         Color oldColor = this.foreground;
         this.foreground = f;
         support.firePropertyChange(PROPERTY_FOREGROUND, oldColor, f);
     }
-    public Color getForeground(){
+
+    public Color getForeground() {
         return this.foreground;
     }
-    
+
     public int getAngle() {
         return angle;
     }
@@ -90,13 +213,13 @@ public class ItemPieMenu {
     public void setAngle(int angle) {
         this.angle = angle;
     }
-    
-    public void privateAddPropertyChange (String propertyName, PropertyChangeListener listener){
+
+    public void privateAddPropertyChange(String propertyName, PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
-    
-    public void privateRemovePropertyChange (String propertyName, PropertyChangeListener listener){
+
+    public void privateRemovePropertyChange(String propertyName, PropertyChangeListener listener) {
         support.removePropertyChangeListener(listener);
     }
-    
+
 }
